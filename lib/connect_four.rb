902cc7c0091a68c0c927a21d2c7638b9f6ead873
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-16 11:41:21 -0600

    adds a few tests. Needs to create piece class test and class

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,3 +1,9 @@
+require './lib/game_board.rb'
+
 # Welcome message
 puts "Welcome to Connect Four! I happen to be a very
-stable computer genius. Think you can beat me?"
+stable computer genius. Think you can beat me?\n\n"
+
+
+connect_four = GameBoard.new
+puts connect_four.original_board
--- test/game_board_test.rb
+++ test/game_board_test.rb
@@ -1,5 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/game_board.rb'
+require 'pry'
 
 class GameBoardTest < MiniTest::Test
 
@@ -9,31 +11,74 @@ class GameBoardTest < MiniTest::Test
     assert_instance_of GameBoard, game_board
   end
 
-  def test_it_starts_with_empty_spaces
+  #This is the integration test
+  def test_it_can_receive_a_piece_and_print_new_board
+    skip
+    game_board = GameBoard.new
 
+    assert_equal expected, actual
   end
-  def test_it_can_check_if_a_column_is_full
 
+  def test_it_rearranges_array_into_columns
+    game_board = GameBoard.new
+    expected = ["ABCDEFG",
+      ".......", ".......",
+      ".......", ".......",
+      ".......", "......."]
+    assert_equal expected, game_board.board_array_transpose
   end
-  def test_it_can_hold_new_pieces
+  def test_it_arranges_array_into_board
+    game_board = GameBoard.new
+    expected = "ABCDEFG\n.......\n.......\n.......\n.......\n.......\n.......\n"
+    assert_equal expected, game_board.original_board
+  end
 
+  def test_it_can_add_new_pieces
+    game_board = GameBoard.new
+    new_piece = GamePiece.new
+
+    assert_equal expected, actual
   end
   def test_it_can_stack_pieces_on_top_of_each_other
+    skip
+    game_board = GameBoard.new
 
+    assert_equal expected, actual
+  end
+  def test_it_can_reject_a_piece_in_a_full_column
+    skip
+    game_board = GameBoard.new
+
+    assert_equal expected, actual
   end
   def test_win_condition_after_4_in_a_row
+    skip
+    game_board = GameBoard.new
 
+    assert_equal expected, actual
   end
   def test_win_condition_after_4_in_a_column
+    skip
+    game_board = GameBoard.new
 
+    assert_equal expected, actual
   end
   def test_win_condition_after_4_diagonal
+    skip
+    game_board = GameBoard.new
 
+    assert_equal expected, actual
   end
   def test_it_can_end_in_a_draw
+    skip
+    game_board = GameBoard.new
 
+    assert_equal expected, actual
   end
   def test_it_print_board_after_a_turn_completes
+    skip
+    game_board = GameBoard.new
 
+    assert_equal expected, actual
   end
 end
