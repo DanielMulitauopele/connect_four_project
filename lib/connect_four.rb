78b92fd626246bddc585aab27e399bf87d6563be
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-16 07:55:49 -0600

    Added tests to game board test file. Still need to fill them in.

---
+++ lib/connect_four.rb
@@ -0,0 +1,3 @@
+# Welcome message
+puts "Welcome to Connect Four! I happen to be a very
+stable computer genius. Think you can beat me?"
--- test/game_board_test.rb
+++ test/game_board_test.rb
@@ -9,4 +9,31 @@ class GameBoardTest < MiniTest::Test
     assert_instance_of GameBoard, game_board
   end
 
+  def test_it_starts_with_empty_spaces
+
+  end
+  def test_it_can_check_if_a_column_is_full
+
+  end
+  def test_it_can_hold_new_pieces
+
+  end
+  def test_it_can_stack_pieces_on_top_of_each_other
+
+  end
+  def test_win_condition_after_4_in_a_row
+
+  end
+  def test_win_condition_after_4_in_a_column
+
+  end
+  def test_win_condition_after_4_diagonal
+
+  end
+  def test_it_can_end_in_a_draw
+
+  end
+  def test_it_print_board_after_a_turn_completes
+
+  end
 end
