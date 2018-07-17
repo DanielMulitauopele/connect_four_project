3ae74573cc2304c63ca2aa22f742c46b25f754fb
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-17 15:34:03 -0600

    creates player test and player file

---
+++ test/player_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+require 'pry'
+
+class PlayerTest < MiniTest::Test
+  def test_it_exists
+    player = Player.new
+
+    assert_instance_of Player, player
+  end
+end 
