10f2826ccb48c124c783426249cd3c24f8476f55
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-17 22:49:23 -0600

    adds make move method to round class

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -22,35 +22,35 @@ class GameBoard
     end
   end
 
-  def print_blank_board
+  def print_board
     final_board = transpose_the_board.map do |row|
       row + "\n"
     end
     final_board.join
   end
 
-  def valid_input?(column)
-    ["A", "B", "C", "D", "E", "F", "G"].include?(column)
+  def valid_input?(column_input)
+    ["A", "B", "C", "D", "E", "F", "G"].include?(column_input)
   end
 
   def valid_space?(array_index)
       board[array_index].include?(".")
   end
 
-  def make_move(column)
-    if column == "A"
+  def make_move(column_input)
+    if column_input == "A"
       @board[0][6] = "X"
-    elsif column == "B"
+    elsif column_input == "B"
       @board[1][6] = "X"
-    elsif column == "C"
+    elsif column_input == "C"
       @board[2][6] = "X"
-    elsif column == "D"
+    elsif column_input == "D"
       @board[3][6] = "X"
-    elsif column == "E"
+    elsif column_input == "E"
       @board[4][6] = "X"
-    elsif column == "F"
+    elsif column_input == "F"
       @board[5][6] = "X"
-    elsif column == "G"
+    elsif column_input == "G"
       @board[6][6] = "X"
     end
   end
