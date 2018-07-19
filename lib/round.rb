29569dc00a697dedac189b7687ac423e7c0fd48f
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-18 20:24:42 -0600

    completes refactoring that monstrosity into smaller methods

--- lib/round.rb
+++ lib/round.rb
@@ -16,47 +16,13 @@ class Round
   def start
     introduction_segment
     while @game_over == false
-        move = receive_move
-      while
-        @gameboard.valid_input?(move) == false
-        print "I think you might have misheard me. I said A-G please. Try again. "
-        move = receive_move
-      end
-        @gameboard.make_move(move, @current_player)
-        puts @gameboard.print_board
-          if @gameboard.horizontal_win? == true || @gameboard.vertical_win? == true
-            puts "You win! Lucky I think. Wanna play again? (enter yes)"
-            @game_over = true
-            if gets.chomp == "yes"
-              start
-            else
-              puts "Fine. See ya!"
-              break
-            end
-          end
-        switch_player
-        puts "\nOk, my turn...hmmmmm..."
-        sleep(3)
-        computer_move = @computer.random_column
-      while
-        @gameboard.valid_input?(computer_move) == false
-        computer_move = @computer.random_column
-      end
-        @gameboard.make_move(computer_move, @current_player)
-        puts @gameboard.print_board
-          if @gameboard.horizontal_win? == true || @gameboard.vertical_win? == true
-            puts "I win! I'll let you try again if you're nice."
-            @game_over = true
-            puts "Wanna play again? Enter 'yes' to get rekt again."
-            if gets.chomp == "yes"
-              start
-            else
-              puts "Fine. See ya!"
-            end
-          else
-            puts following_request_move
-          end
-        switch_player
+      normal_player_gameplay
+      did_the_player_win
+      break if @game_over == true
+      switch_player
+      normal_computer_gameplay
+      did_the_computer_win
+      switch_player
     end
   end
 
@@ -67,6 +33,57 @@ class Round
     puts welcome_message
     puts gameboard.print_board
     print first_request_move
+  end
+
+  def normal_player_gameplay
+    move = receive_move
+    while
+      @gameboard.valid_input?(move) == false
+      print "I think you might have misheard me. I said A-G please. Try again. "
+      move = receive_move
+    end
+      @gameboard.make_move(move, @current_player)
+      puts @gameboard.print_board
+  end
+
+  def normal_computer_gameplay
+      puts "\nOk, my turn...hmmmmm..."
+      sleep(2)
+      computer_move = @computer.random_column
+    while
+      @gameboard.valid_input?(computer_move) == false
+      computer_move = @computer.random_column
+    end
+      @gameboard.make_move(computer_move, @current_player)
+      puts @gameboard.print_board
+  end
+
+
+  def did_the_player_win
+    if @gameboard.horizontal_win? == true || @gameboard.vertical_win? == true
+      puts "You win! Lucky I think. Wanna play again? (enter yes)"
+      @game_over = true
+      if gets.chomp == "yes"
+        start
+      else
+        puts "Fine. See ya!"
+      end
+    end
+  end
+
+  def did_the_computer_win
+    if @gameboard.horizontal_win? == true || @gameboard.vertical_win? == true
+      puts "I win! I'll let you try again if you're nice."
+      @game_over = true
+      puts "Wanna play again? Enter 'yes' to get rekt again."
+      if gets.chomp == "yes"
+        start
+      else
+        puts "Fine. See ya!"
+      end
+    else
+      puts following_request_move
+    end
   end
 
   def welcome_message
